04d77e211ec5a53b9650ec6376d0174ba71d3f8b
46Ashish1 <120231235+46Ashish1@users.noreply.github.com>
2024-05-09 01:29:33 +0530

    bezier.cpp

--- bezier.cpp
+++ bezier.cpp
@@ -1,29 +1,36 @@
-#include <graphics.h>
-#include <math.h>
 #include <conio.h>
+#include <graphics.h>
+#include <iostream.h>
+#include <math.h>
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+void bezier(int x[4], int y[4])
 {
-    int x[4], y[4], i;
-    double put_x, put_y, t;
-    int gr = DETECT, gm;
-    initgraph(&gr, &gm, NULL);
-    printf("\n****** Bezier Curve ***********");
-    printf("\n Please enter x and y coordinates ");
-    for (i = 0; i < 4; i++)
+    for (double t = 0.0; t < 1.0; t += 0.00005)
     {
-        scanf("%d%d", &x[i], &y[i]);
-        putpixel(x[i], y[i], 3); // Control Points
+        double xt = pow(1 - t, 3) * x[0] + 3 * t * pow(1 - t, 2) * x[1] + 3 * pow(t, 2) * (1 - t) * x[2] + pow(t, 3) * x[3];
+        double yt = pow(1 - t, 3) * y[0] + 3 * t * pow(1 - t, 2) * y[1] + 3 * pow(t, 2) * (1 - t) * y[2] + pow(t, 3) * y[3];
+        putpixel(xt, yt, WHITE);
     }
-
-    for (t = 0.0; t <= 1.0; t = t + 0.001) // t always lies between 0 and 1
+    for (int i = 0; i < 4; i++)
     {
-        put_x = pow(1 - t, 3) * x[0] + 3 * t * pow(1 - t, 2) * x[1] + 3 * t * t * (1 - t) * x[2] + pow(t, 3) * x[3]; // Formula to draw curve
-        put_y = pow(1 - t, 3) * y[0] + 3 * t * pow(1 - t, 2) * y[1] + 3 * t * t * (1 - t) * y[2] + pow(t, 3) * y[3];
-        putpixel(put_x, put_y, WHITE); // putting pixel
+        circle(x[i], y[i], 3);
     }
     getch();
     closegraph();
-
-    return 0;
+    return;
+}
+void main()
+{
+    int i;
+    int x[4], y[4];
+    int gd = DETECT, gm, errorcode;
+    initgraph(&gd, &gm, "..\\bgi");
+    for (i = 0; i < 4; i++)
+    {
+        cout << "Enter Point " << i + 1 << " (x, y): ";
+        cin >> x[i] >> y[i];
+    }
+    bezier(x, y);
+    return;
 }
